6e592d45050e538fb98fd957d71456e3d9132d00
Victor Hugo <vct.bta@gmail.com>
2017-06-11 15:37:15 -0300

    Reestrutura componente, removendo o template de seu interior
    O template no interior do html do componente impedia que a função _render fosse executada mais de uma vez

--- assets/js/dynamic-list.js
+++ assets/js/dynamic-list.js
@@ -32,12 +32,14 @@ var dynamic_list = (function($){
       }
     ]
   },
-  $dynamic_list = $('#dynamic-list');
+  $dynamic_list = $('#dynamic-list'),
+  $dynamic_list_ul = $dynamic_list.children('ul'),
+  $dynamic_list_template = $dynamic_list.children('#dynamic-list__template');
 
   _render();
 
   function _render() {
-    $dynamic_list.html(Mustache.render($dynamic_list.html(), data));
+    $dynamic_list_ul.html(Mustache.render($dynamic_list_template.html(), data));
   }
 
   return {};
