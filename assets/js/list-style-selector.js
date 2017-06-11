d881dda631758e299c67a3b30fc681304b5f6b2f
Victor Hugo <vct.bta@gmail.com>
2017-06-11 17:12:35 -0300

    Cria o módulo list_style_selector

---
+++ assets/js/list-style-selector.js
@@ -0,0 +1,34 @@
+var list_style_selector = (function($){
+  'use strict';
+  var data = {
+    li: [
+      {
+        title: 'BLOCK',
+        style: 'block'
+      },
+      {
+        title: 'LIST',
+        style: 'inline'
+      }
+    ]
+  },
+  $list_style_selector = $('#list-style-selector'),
+  $list_style_selector_ul = $list_style_selector.children('ul'),
+  $list_style_selector_template = $list_style_selector.children('#list-style-selector__template');
+
+  _render();
+
+  $list_style_selector.on('click', 'button', do_click_action);
+
+  function _render() {
+    $list_style_selector_ul.html(Mustache.render($list_style_selector_template.html(), data));
+  }
+
+  function do_click_action() {
+    var $this = $(this);
+
+    PubSub.publish('change_list_style', $this.data('style-type'));
+  }
+
+  return {};
+})(jQuery);
